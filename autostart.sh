bf05cfdd7bfcee80e574082fe1531fd16da41508
Daniel <12125454+danieljudd@users.noreply.github.com>
2021-10-27 13:22:49 +0100

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,19 @@
+#    Copyright (C) 2021  Daniel Judd, https://github.com/danieljudd AND
+#    danieljudd.xyz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/usr/bin/env bash
 
 # This file [ "$HOME/.dwm" ]
@@ -7,7 +23,7 @@
 exec nm-applet &
 
 # Mount network drive
-sshfs dietpi@192.168.1.244:/mnt/external-drive /mnt/sftp/
+# sshfs (username@ip):/mnt/LANdrive /mnt/localdrive/
 
 # monitor standby | suspend | off
 exec xset dpms 3600 4500 0 &
