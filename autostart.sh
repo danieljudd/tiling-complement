486f4e0c714520822594e38036bb2ec019114e05
Daniel <12125454+danieljudd@users.noreply.github.com>
2022-02-01 13:41:48 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,22 +1,7 @@
-#    Copyright (C) 2021  Daniel Judd, https://github.com/danieljudd
-#    danieljudd.xyz
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 #!/usr/bin/env bash
 
 # This file may go here [ "$HOME/.dwm" ]
+# Made by danieljudd.github.com
 
 # Network management tray applet
 # 	Use nm-tray in Debian if there is no nm-applet installed
@@ -30,7 +15,7 @@ exec blueberry-tray &
 
 # monitor standby | suspend | off
 exec xset dpms 3600 4500 0 &
-# screensaver off
+# screensaver off because it runs during fullscreen video
 exec xset s off &
 
 # allows java programs to run
@@ -45,15 +30,16 @@ WEATHER=$(curl wttr.in/Cardiff?format="%l:+%m+%p+%w+%t+%c+%C")
 while true; do
 	LOCALTIME=$(date +'%A, %d-%m-%Y %H:%M')
         MEM=$(free -h --kilo | awk '/^Mem:/ {print $3 "/" $2}')
-        #LINUX=$(uname -r)
-        #TOTALDOWN=$(sudo ifconfig wlan0 | grep "RX packets" | awk {'print $6 $7'})
-        #TOTALUP=$(sudo ifconfig wlan0 | grep "TX packets" | awk {'print $6 $7'})
+        # LINUX=$(uname -r)
+        # TOTALDOWN=$(sudo ifconfig wlan0 | grep "RX packets" | awk {'print $6 $7'})
+        # TOTALUP=$(sudo ifconfig wlan0 | grep "TX packets" | awk {'print $6 $7'})
         CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
         VOL=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))
-        BAT=$(cat '/sys/class/power_supply/BAT0/capacity')
+        # Enable if you have a battery (laptop) and add "BAT: $BAT%" to xsetroot list
+	# BAT=$(cat '/sys/class/power_supply/BAT0/capacity')
         # Check Weather every half hour
         # !! Update location here !!
         if [ $(date +'%M') == 30 ] || [ $(date +'%M') == 00 ]; then WEATHER=$(curl wttr.in/Cardiff?format="%l:+%m+%p+%w+%t+%c+%C"); fi
-        xsetroot -name " $LOCALTIME | MEM: $MEM | CPU: $CPU | $WEATHER | VOL: $VOL | BAT: $BAT%"
+        xsetroot -name " $LOCALTIME | MEM: $MEM | CPU: $CPU | $WEATHER | VOL: $VOL"
         sleep 10s
 done &
